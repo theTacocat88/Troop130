b5168412704a29544b98ac6967106aee4331f192
Elias Glick <eliasglick1@gmail.com>
2025-12-20 21:20:57 -0500

    script.js functionality, calendar functions

--- src/script.js
+++ src/script.js
@@ -2,8 +2,13 @@ const calendar = document.getElementById("event-calendar");
 const dates = document.getElementsByClassName("cal-day");
 const event_div = document.getElementById("event-details");
 const event_details = document.getElementById("event-details-content");
+const event_details_heading = document.getElementById("event-details-heading");
+const calendar_heading = document.getElementById("cal-heading");
 
 var eventDetails;
+var today = new Date();
+var month = today.getMonth();
+var year = today.getFullYear();
 
 var events = {
     "1": "Weekly troop meeting, 7:00 PM",
@@ -39,6 +44,19 @@ var events = {
     "31": ""
 }
 
+function getSuffixOfDate(date) {
+  switch(date) {
+    case 1: return "st";
+    case 2: return "nd";
+    case 3: return "rd";
+    case 21: return "st";
+    case 22: return "nd";
+    case 23: return "rd";
+    case 31: return "st";
+    default: return "th";
+  }
+}
+
 function displayEventDetails(event) {
   const eventId = event.target.getAttribute("data-date-id");
   if(events[eventId] != "") {
@@ -47,6 +65,7 @@ function displayEventDetails(event) {
     eventDetails = "No events scheduled for this day";
   }
   event_details.innerHTML = eventDetails;
+  event_details_heading.innerHTML = convertMonthToString(month) + " " + eventId + getSuffixOfDate(eventId);
 }
 
 function addEventListeners() {
@@ -54,4 +73,27 @@ function addEventListeners() {
         dates[i].addEventListener("click", displayEventDetails);
     }
 }
+
+function convertMonthToString(month) {
+  switch(month) {
+    case 0: return "January";
+    case 1: return "February";
+    case 2: return "March";
+    case 3: return "April";
+    case 4: return "May";
+    case 5: return "June";
+    case 6: return "July";
+    case 7: return "August";
+    case 8: return "September";
+    case 9: return "October";
+    case 10: return "November";
+    case 11: return "December";
+  }
+}
+
+function setMonth(month, year) {
+  calendar_heading.innerHTML = convertMonthToString(month) + " " + year;
+}
+
 addEventListeners();
+setMonth(month, year);
