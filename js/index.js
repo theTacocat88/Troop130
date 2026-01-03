ee25f352cb3b6c568549098855ba7d254f5c8176
Elias Glick <eliasglick1@gmail.com>
2026-01-03 11:47:42 -0500

    fix: add alt='' to each placeholder image in homepage gallery

--- js/index.js
+++ js/index.js
@@ -3,21 +3,21 @@ const rightGalleryButton = document.getElementById("right-button-gallery");
 const galleryItem = document.getElementById("gallery-item");
 
 var gallery = [
-  "<img src='https://placehold.co/900x600?text=Placeholder+1' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+2' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+3' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+4' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+5' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+6' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+7' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+8' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+9' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+10' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+11' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+12' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+13' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+14' loading='eager'/>",
-  "<img src='https://placehold.co/900x600?text=Placeholder+15' loading='eager'/>"
+  "<img src='https://placehold.co/900x600?text=Placeholder+1' loading='eager' alt='1'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+2' loading='eager' alt='2'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+3' loading='eager' alt='3'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+4' loading='eager' alt='4'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+5' loading='eager' alt='5'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+6' loading='eager' alt='6'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+7' loading='eager' alt='7'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+8' loading='eager' alt='8'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+9' loading='eager' alt='9'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+10' loading='eager' alt='10'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+11' loading='eager' alt='11'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+12' loading='eager' alt='12'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+13' loading='eager' alt='13'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+14' loading='eager' alt='14'/>",
+  "<img src='https://placehold.co/900x600?text=Placeholder+15' loading='eager' alt='15'/>"
 ];
 
 function addEventListeners() {
